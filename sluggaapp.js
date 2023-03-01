0f38cf6942d09532a4ba7b9616db0bb3e4abca5a
Slugga Farm <125496910+sluggafarm@users.noreply.github.com>
2023-03-01 14:01:40 -0500

    Update sluggaapp.js

--- sluggaapp.js
+++ sluggaapp.js
@@ -1,1 +1,44 @@
-
+var sluggaapp = {
+          wallets : [], 
+          onload: function () {
+            let wallet = "";
+            console.log('loading slugga app...');
+            if (window.localStorage) { 
+              wallet = window.localStorage.getItem('SluggaFarm_MRU_Wallet'); 
+              if (wallet) {
+                sluggaapp.connectWallet(); 
+              }
+            }          
+            document.getElementById('connectwalletbutton').addEventListener("click", function () { sluggaapp.connectWallet(); });
+            document.getElementById('disconnectwalletbutton').addEventListener("click", function () { sluggaapp.disconnectWallet(); });
+          },
+          disconnectWallet : function () {
+            window.localStorage.setItem('SluggaFarm_MRU_Wallet', "");
+            sluggaapp.wallets = [];
+            document.getElementById('connect-button-panel').style.display="";
+            document.getElementById('connected-panel').style.display="none";
+            document.querySelector('figure img').style.opacity="1.0";
+            document.querySelector('.lead').style.display="";
+          },
+          connectWallet: function () {
+              if (window.ethereum) {
+                  let eth = window.ethereum;
+                  eth.request({ method: 'eth_requestAccounts' })
+                      .then(walletsdata => {
+                          sluggaapp.wallets = walletsdata;
+                          if (window.localStorage) { window.localStorage.setItem('SluggaFarm_MRU_Wallet', walletsdata[0]); }
+                          document.getElementById('walletLabel').innerText = sluggaapp.walletMask(sluggaapp.wallets[0]);
+                          document.getElementById('connect-button-panel').style.display="none";
+                          document.querySelector('.lead').style.display="none";
+                          document.getElementById('connected-panel').style.display="";
+                          document.querySelector('figure img').style.opacity="0.05";
+                      }).catch(err => { alert(err.message); });
+              } else {
+                  alert('cannot connect to MetaMask plugin.');
+              }
+          },
+           walletMask : function (s) { 
+               return s.substring(0, 6) + "...." + s.substring(s.length - 7);
+           }
+        }
+        window.setTimeout(function () { sluggaapp.onload(); }, 125);
